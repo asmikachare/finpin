cec68a0d43cb09c845844fc59da1824b6c7f8227
Asmi Kachare <asmikachare@gmail.com>
2025-10-26 23:18:38 -0700

    Initial Commit

---
+++ Finpin/FinpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinpinApp.swift
+//  Finpin
+//
+//  Created by Asmi Kachare on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FinpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinpinTests/FinpinTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinpinTests.swift
+//  FinpinTests
+//
+//  Created by Asmi Kachare on 10/26/25.
+//
+
+import Testing
+@testable import Finpin
+
+struct FinpinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
